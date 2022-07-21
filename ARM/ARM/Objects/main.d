750af13da5004ecc1cd0ea42d68d9cf4d0886181
Aash <aash.shah@somaiya.edu>
2022-07-21 13:55:51 +0530

    first commit

---
+++ ARM/ARM/Objects/main.d
@@ -0,0 +1,6 @@
+.\Objects\main.o: main.c
+.\Objects\main.o: C:\Keil\ARM\INC\Philips\lpc214x.h
+.\Objects\main.o: C:\Keil\ARM\RV31\INC\stdio.h
+.\Objects\main.o: LCD.h
+.\Objects\main.o: ADC.h
+.\Objects\main.o: UART.h
